f7db74a2ab8612e6f503de2799e6886fd6cb73ea
terry00123 <terry00123@gmail.com>
2019-01-01 18:28:42 +0900

    multiPlayer 3.0

--- app/src/main/java/com/example/q/myapplication/Tab3etc.java
+++ app/src/main/java/com/example/q/myapplication/Tab3etc.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class Tab3etc extends Fragment {
@@ -13,8 +14,15 @@ public class Tab3etc extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab3etc, container, false);
-        Intent intent = new Intent(getActivity(), PingPong.class);
-        startActivity(intent);
+
+        FrameLayout frame1 = rootView.findViewById(R.id.frame1);
+        frame1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PingPong.class);
+                startActivity(intent);
+            }
+        });
         return rootView;
     }
 }
